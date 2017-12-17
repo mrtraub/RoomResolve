c0f563f2070ef0923233a7b9b6b49bb8a27ce9f6
logan <loganriddick@yahoo.com>
2017-12-17 17:10:27 -0500

    Correct MATLAB code for HW6
    Works just about perfectly. Enjoy

--- HW6.m
+++ HW6.m
@@ -1,11 +1,11 @@
 %Homework 6 
-fcr1 = 323;
-fcr2 = 649;
-f1 = 125:1:fcr1;
-f2 = fcr2:1:2000;
-f=125:1:2000;
-m1 = 30.5;
-rho = 600;
+fcr = 1330;
+f1 = 125:1:fcr;
+f2 = fcr:1:2000;
+f = 125:1:2001;
+w = 2*pi*f;
+m1 = 7.62;
+rho = 1.21;
 c = 345;
 w1 = 2*pi*f1;
 w2 = 2*pi*f2;
@@ -14,26 +14,29 @@ h = 0.0127;
 cl = 3873;
 n = 1e-2;
 
-fcr = 323;
-
 R1 = 20*log10((m1 * w1)/(2*rho*c)) - 3;
-R2 = 20*log10((m1*w2)/(2*rho*c))+5*log10(f2/fcr2)+10*log10(2*n);
+R2 = 20*log10((m1*w2)/(2*rho*c))+5*log10(f2/fcr)+10*log10(2*n);
+R = [R1 R2];
 
-plot(f1,R1);
+semilogx(f,R)
 title('Loss Factor vs Freq')
 xlabel('Frequency (Hz)')
 ylabel('R (dB)')
 grid on
-hold on
-plot(f2,R2)
 
+%double leaf partition
+m1 = 15.2;
 m2 = 30.5;
 rho0 = 1.21;
-d = 0.05;
-w0 = (rho0*c*c/d)/m2;
+d = 0.1524;
+s = rho*c*c/d;
+w0 =s/m2;
 
-Rnew1 = R1 + 10*log10(1-((w1.*w1)/(w0*w0))).^2;
-Rnew2 = R2 + 10*log10(1-((w2.*w2)/(w0*w0))).^2;
+R1 = 20*log10((m1 * w1)/(2*rho*c)) - 3;
+R2 = 20*log10((m1*w2)/(2*rho*c))+5*log10(f2/fcr)+10*log10(2*n);
+R = [R1 R2];
+
+Rnew = R + 10*log10((1-w.*w/w0).*(1-w.*w/w0));
+
 hold on
-plot(f1,Rnew1)
-plot(f2,Rnew2)
+semilogx(f,Rnew)
